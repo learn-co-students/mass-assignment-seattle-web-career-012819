909f616a6ae176ac8e0276fe7422ee4e9bb52c12
github username <github email address>
2019-02-07 13:19:23 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -6,19 +6,10 @@ class Person
   :wrist_size, :glove_size, :pant_width, :pant_length
 
   def initialize(attributes)
-    @name = attributes[:name]
-    @birthday = attributes[:birthday]
-    @hair_color = attributes[:hair_color]
-    @eye_color = attributes[:eye_color]
-    @height = attributes[:height]
-    @weight = attributes[:weight]
-    @handed = attributes[:handed]
-    @complexion = attributes[:complexion]
-    @t_shirt_size = attributes[:t_shirt_size]
-    @wrist_size = attributes[:wrist_size]
-    @glove_size = attributes[:glove_size]
-    @pant_length = attributes[:pant_length]
-    @pant_width = attributes[:pant_width]
+    #binding.pry
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
   end
 
 end
