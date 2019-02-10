9d18cc8fadbd3085c7210e784b5eec7da35772e4
github username <github email address>
2019-02-10 13:29:48 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color
+  attr_accessor :height, :weight, :handed, :complexion, :t_shirt_size
+  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(args)
+    args.each {|k, v| self.send("#{k}=", v) }
+  end
 end
